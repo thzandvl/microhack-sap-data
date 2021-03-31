cf8bd1f5b24d6361bb1732cc9356d663fff3d98b
Thijs Zandvliet <66480444+thzandvl@users.noreply.github.com>
2021-03-31 16:57:47 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -67,7 +67,7 @@ resource "azurerm_subnet" "subnet" {
   name                 = "${var.prefix}-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.20.1.0/24"]
+  address_prefixes     = var.subnet_prefixes
 }
 
 #######################################################################
