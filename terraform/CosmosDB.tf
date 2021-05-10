21d281718a492be31c9d3b77f69b984566c16c1c
Thijs Zandvliet <66480444+thzandvl@users.noreply.github.com>
2021-05-10 15:32:30 +0200

    Update CosmosDB.tf
    Updated values

--- terraform/CosmosDB.tf
+++ terraform/CosmosDB.tf
@@ -1,37 +1,64 @@
-#Create CosmosDB Account
-resource "azurerm_cosmosdb_account" "acc" {
-  name                = "${var.prefix}-cosmos"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  offer_type          = "Standard"
-  kind                = "GlobalDocumentDB"
-  tags                = var.tags
+#######################################################################
+## Create CosmosDB Account
+#######################################################################
 
+resource "azurerm_cosmosdb_account" "cosmos" {
+  name                      = "${var.prefix}-cosmos-${lower(random_id.id.hex)}"
+  resource_group_name       = azurerm_resource_group.rg.name
+  location                  = azurerm_resource_group.rg.location
+  offer_type                = "Standard"
+  kind                      = "GlobalDocumentDB"
   enable_automatic_failover = false
+
   consistency_policy {
     consistency_level       = "Session"
+    max_interval_in_seconds = 5
+    max_staleness_prefix    = 100
   }
 
   geo_location {
-    location          = var.location
+    location          = azurerm_resource_group.rg.location
     failover_priority = 0
   }
 }
 
-#Create SQL Database
+#######################################################################
+## Create CosmosDB SQL DB
+#######################################################################
+
 resource "azurerm_cosmosdb_sql_database" "db" {
-  name                = "SAPS4D"
-  resource_group_name = azurerm_cosmosdb_account.acc.resource_group_name
-  account_name        = azurerm_cosmosdb_account.acc.name
+  name                = "SAP${var.SID}"
+  resource_group_name = azurerm_cosmosdb_account.cosmos.resource_group_name
+  account_name        = azurerm_cosmosdb_account.cosmos.name
+  throughput          = 400
 }
 
-#Create Collection
-resource "azurerm_cosmosdb_sql_container" "paymentData" {
+#######################################################################
+## Create CosmosDB SQL Container
+#######################################################################
+
+resource "azurerm_cosmosdb_sql_container" "container" {
   name                  = "paymentData"
-  resource_group_name   = azurerm_cosmosdb_account.acc.resource_group_name
-  account_name          = azurerm_cosmosdb_account.acc.name
+  resource_group_name   = azurerm_cosmosdb_account.cosmos.resource_group_name
+  account_name          = azurerm_cosmosdb_account.cosmos.name
   database_name         = azurerm_cosmosdb_sql_database.db.name
   partition_key_path    = "/CustomerNr"
   partition_key_version = 1
   throughput            = 400
+
+  indexing_policy {
+    indexing_mode = "Consistent"
+
+    included_path {
+      path = "/*"
+    }
+
+    excluded_path {
+      path = "/\"_etag\"/?"
+    }
+  }
+
+  unique_key {
+    paths = ["/CustomerNr"]
+  }
 }
