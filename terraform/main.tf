758e084191ace6b412a3f4f9ea6cdaad98d06098
Thijs Zandvliet <thzandvl@microsoft.com>
2022-03-30 12:59:18 +0200

    updated keyvault to latest syntax

--- terraform/main.tf
+++ terraform/main.tf
@@ -100,3 +100,48 @@ resource "azurerm_subnet_network_security_group_association" "nsg-ass" {
   subnet_id                 = azurerm_subnet.subnet.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
+
+#######################################################################
+## Create Key Vault
+#######################################################################
+
+resource "azurerm_key_vault" "keyvault" {
+  name                  = "sapkv${lower(random_id.id.hex)}"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  tenant_id             = data.azurerm_client_config.user.tenant_id
+  sku_name              = "standard"
+  tags                  = var.tags
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.user.tenant_id
+    object_id = local.object_id
+
+    key_permissions = [
+      "Create",
+      "Get",
+      "Purge",
+      "List",
+      "Delete"
+    ]
+
+    secret_permissions = [
+      "Set",
+      "Get",
+      "Delete",
+      "Purge",
+      "Recover",
+      "List"
+    ]
+  }
+}
+
+#######################################################################
+## Add secret to Key Vault
+#######################################################################
+
+resource "azurerm_key_vault_secret" "secret" {
+  name         = var.username
+  value        = var.password
+  key_vault_id = azurerm_key_vault.keyvault.id
+}
